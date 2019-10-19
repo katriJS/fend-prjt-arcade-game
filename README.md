eb26e01283680ac4f83f6a9e6ce84e5f045b8684
Katrina <katrinajanais@gmail.com>
2019-10-19 09:10:54 -0700

    feat: Game Play
    Created Asset class to handle basic properties and methods for all game 
    asset types.
    	Modified Player and Enemy objects to inherit from Asset.
    	Added logic for game reset. Resets when player is hit by enemy.
    Added Player wins modal. If player reaches the top of the canvas (water) 
    display modal before game reset. Updated Readme.

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-# Classic Arcade Game Clone Project
+# Classic Arcade Game Clone Project - Frogger
 
 ## Table of Contents
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
+* [Game Play](#gameplay)
+* [Installation](#installation)
+* [Instructions](#instructions)
+* [Contributing](#contributing)
 
-## Instructions
+This game is a simple take on the classic Frogger Aracade game. It is based in JavaScript. This project was created as part of the [Udacity Front-end Nanaodegree] program.
 
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
 
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+## Game Play
+ This is a one player game. The goal of the player is to reach the water, without colliding into any one of the enemy bugs. The player can move left, right, up and down. The enemies will move at varying speeds but only on the paved block portion of the scene. When the player collides with an enemy, the player moves back to the starting block. Once the player reaches the water the game is won.
 
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+ Players have the option to select their character before the game starts and change their selection at any time during game play.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## Installation
+
+ Download the [zip] from GitHub or clone from the repository
+ ```sh
+ $ git clone https://github.com/katriJS/fend-prjt-arcade-game.git
+ ```
 
 ## Contributing
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const canvas = document.querySelector('canvas');
+const playerModal = document.getElementById('playerModal');
+const gameOverModal = document.getElementById('gameoverModal');
+
 const sprites = {
         'boy': 'images/char-boy.png',
         'horn girl': 'images/char-horn-girl.png',
@@ -6,52 +9,120 @@ const sprites = {
         'princess girl': 'images/char-princess-girl.png'
     };
 
+const SCREEN_WIDTH = 400;
+let GAME_START =  false;
+
 /**
-* @description Enemies our player must avoid
+* @description constructor for all basic game assets. Player and Enemy objects
+*              inherit from this class
 * @constructor
-* @param {string} ? - ?
-* @param {string} ? - ?
+* @param {int} x - starting x position on the destination canvas
+* @param {int} y - starting y position on the destination canvas
+* @param {int} w - width of the asset. May differ from actual dimensions
+* @param {int} h - height of the asset. May differ from actual dimensions
+* @param {int} src - source of the asset image.
 */
-const Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const Asset = function(x, y, w, h, src) {
+  // The image/sprite for assets, this uses
+  // a helper provided to easily load images
+    this.sprite = src;
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.start_position = {x: this.x, y: this.y};
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+// Draw the asset on the screen, required method for game
+Asset.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// reposition the asset to its original position
+Asset.prototype.reset = function() {
+
+  this.x = this.start_position.x;
+  this.y = this.start_position.y;
+};
+
+// Detect if a collision occur with the given sprite
+Asset.prototype.collisionDetected = function(sprite) {
+  let x_min = this.x - this.width;
+  let x_max = this.x + this.width;
+  let y_min = this.y - this.height;
+  let y_max = this.y + this.height;
+
+  if(sprite.x > x_min && sprite.x <= x_max &&
+     sprite.y > y_min && sprite.y <= y_max) {
+       console.log(Math.floor(x_min),' < p-x: ',sprite.x,' < ',Math.floor(x_max));
+       console.log(Math.floor(y_min),' < p-y: ',sprite.y,' < ',Math.floor(y_max));
+       return true;
+  }
+    return false;
+};
+
 /**
 * @description Enemies our player must avoid
 * @constructor
-* @param {string} ? - ?
-* @param {string} ? - ?
+* inherits from Asset
 */
-const Player = function() {
+function Enemy(x, y, w, h, src)  {
+    //call the supertype constructor
+    Asset.call(this, x, y, w, h, src);
+    this.speed = 100;
+}
 
-    // The image/sprite for our player
-    this.sprite = 'images/char-boy.png';
-    this.x = 200;
-    this.y = 400;
+Enemy.prototype = Object.create(Asset.prototype, {
+                  constructor: {
+                      configurable: true,
+                      enumerable: true,
+                      value: Enemy,
+                      writable: true
+                  }
+});
 
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt, player) {
+    // Multiply any movement by the dt parameter to ensure the
+    // game runs at the same speed for all computers.
+    if(GAME_START) {
+
+      this.x += this.speed * dt;
+      if(this.x > SCREEN_WIDTH){
+        this.x = -100; //off screen
+        this.speed = 50 + Math.floor(Math.random() * 201);
+      }
+
+      if(this.collisionDetected(player)) {
+        player.hit = true;
+      }
+    }
 
 };
+
+
+
+/**
+* @description Player class. This class requires an update(), render() and
+*              a handleInput() method.
+* @constructor
+* Inherits from Asset
+*/
+function Player (x, y, w, h, src) {
+    //call the supertype constructor
+    Asset.call(this, x, y, w, h, src);
+    this.hit = false;
+}
+
+Player.prototype = Object.create(Asset.prototype, {
+                  constructor: {
+                      configurable: true,
+                      enumerable: true,
+                      value: Player,
+                      writable: true
+                  }
+});
 
 //Choose a player
 Player.prototype.switch = function(selection, sprites) {
@@ -60,44 +131,44 @@ Player.prototype.switch = function(selection, sprites) {
       let spriteKeys = Object.values(sprites);
       let currentIndex = spriteKeys.indexOf(this.sprite);
       nextSprite = currentIndex == spriteKeys.length-1 ? 0 : currentIndex+1;
-      console.log(spriteKeys[nextSprite]);
       this.sprite = spriteKeys[nextSprite];
       break;
 
     default:
       this.sprite = sprites[selection.alt];
-      console.log(sprites[selection.alt]);
       break;
   }
 };
 
 // Update player's position, required method for game
 Player.prototype.update = function() {
-  console.log('x: ',this.x,' y: ',this.y);
-};
-
-// Update player's position, required method for game
-Player.prototype.collisionDetected = function(element) {
-  console.log('here');
-  console.log('element: x',element.x,' y: ',element.y);
-  if(this.x === element.x && this.y === element.y){
-    return true;
+  if (this.hit) {
+    //feedback("You Lose!");
+    this.reset();
+    //GAME_START = false;
+  }
+  else {
+    if(this.y < 0) {
+        feedback("You Win!");
+        this.reset();
+        GAME_START = false;
+    }
   }
 
 };
 
-// Draw the player on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+Player.prototype.reset = function() {
+  Asset.prototype.reset.call(this);
+  this.hit = false;
 };
 
 Player.prototype.handleInput = function(value) {
-    console.log(value);
-
 
     switch (value) {
       case 'up':
           this.y = this.y > 0  ? this.y-90 : this.y;
+          if(this.y < 0) this.y = -10;
         break;
 
       case 'down':
@@ -113,57 +184,81 @@ Player.prototype.handleInput = function(value) {
         break;
 
       case 'space':
-          // TODO: if player position overlaps with selector change sprites
           if(this.collisionDetected(selector)){
             this.switch('next', sprites);
           }
-
         break;
-      default:
 
     }
 
 };
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+
+
+// Instantiate all objects.
+const player = new Player(200, 400, 50, 85, 'images/char-boy.png');
+
+const allEnemies = [new Enemy(10, 65, 70, 60, 'images/enemy-bug.png'),
+                    new Enemy(10, 150, 70, 60, 'images/enemy-bug.png'),
+                    new Enemy(10, 230, 70, 60, 'images/enemy-bug.png')];
+
+const selector = new Asset(200, 400, 50, 42, 'images/Selector.png');
+let button = playerModal.querySelector('button');
+button.focus();
+
+
+
 /**
-* @description Enemies our player must avoid
+* @description handled input for player selection
 * @constructor
-* @param {string} ? - ?
-* @param {string} ? - ?
+* @param {string/element} selection - 'default' string or doc element/sprite
 */
-const Item = function(x, y, src) {
-    this.sprite = src;
-    this.x = x;
-    this.y = y;
-};
-
-// Draw the item on the screen, required method for game
-Item.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-const player = new Player();
-const allEnemies = [];
-const selector = new Item(200, 400, 'images/Selector.png');
-const modal = document.querySelector('.modal');
-
-
 function selectPlayer(selection) {
+  GAME_START = true;
+
+
   if(selection === 'default') {
     selection = document.getElementById('default');
   }
+
   player.switch(selection, sprites);
 
   //Hide modal
-  modal.style.display = 'none';
+  playerModal.style.display = 'none';
 
+}
+
+/**
+* @description Win or Lose Modal feedback
+* @param {'string'} m - the message to display in Modal
+*/
+function feedback(msg) {
+
+  let message = gameOverModal.querySelector('h2');
+  let button = gameOverModal.querySelector('button');
+  message.textContent = msg;
+
+  canvas.style.filter = 'blur(5px)';
+  gameOverModal.style.display = 'flex';
+  button.focus();
+
+}
+
+
+/**
+* @description Reset Game
+*
+*/
+function resetGame() {
+  allEnemies.forEach(function(enemy) {
+      enemy.reset();
+  });
+  player.reset();
+  canvas.style.filter = 'unset';
+  gameOverModal.style.display = 'none';
+  GAME_START = true;
 }
 
 // This listens for key presses and sends the keys to your
@@ -177,11 +272,13 @@ document.addEventListener('keyup', function(e) {
         32: 'space'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    if(GAME_START)
+      player.handleInput(allowedKeys[e.keyCode]);
+
 });
 
 window.onclick = function(event) {
- if (event.target == modal) {
+ if (event.target == playerModal) {
    selectPlayer('default');
  }
 };
